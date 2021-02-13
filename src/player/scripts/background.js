94820dd07f185b7955296fbc4f9e46f94ad53300
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-02-13 17:33:31 +0530

    basic communication setup between extenion and content scriopt which runs in the context of webpage

--- src/player/scripts/background.js
+++ src/player/scripts/background.js
@@ -1,1 +1,14 @@
+// @ts-nocheck
 console.log('from background---');
+
+$('#startGLS').on('click', function () {
+  console.log('hello--- back');
+
+  // start gls
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log('sending message from back...');
+    chrome.tabs.sendMessage(tabs[0].id, 'Start GLS', function (response) {
+      console.log(response);
+    });
+  });
+});
--- src/player/scripts/content.js
+++ src/player/scripts/content.js
@@ -1,1 +1,15 @@
-console.log('hello there---');
+// @ts-nocheck
+console.log('hello there from content---');
+
+// listen for request to start GLS
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('got a message ---');
+  console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
+  console.log(request);
+  sendResponse('Starting GLS ...');
+});
+
+// when GLS has completed send message to extension
+// chrome.runtime.sendMessage('GLS is completed.', function (response) {
+//   console.log(response);
+// });
