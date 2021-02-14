d60aef0a8d476cff91b8639c18a94caf565874f8
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-02-14 18:52:59 +0530

    add new step for voice search, added comments

--- src/player/scripts/content.js
+++ src/player/scripts/content.js
@@ -1,50 +1,55 @@
-console.log('hello there from content---');
-
 // listen for request to start GLS
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // console.log('got a message ---');
-  // console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
-  console.log('New message from extension : ', request);
-  sendResponse('Starting GLS ...');
+  sendResponse({ message: 'Starting GLS ...', status: 'success' });
   startGLS();
 });
 
-// when GLS has completed send message to extension
-// chrome.runtime.sendMessage('GLS is completed.', function (response) {
-//   console.log(response);
-// });
-
+// tooltip creation, handle back/next/cancel operations
 function startGLS() {
-  console.log('working on gls ...');
+  console.log('GLS in progress ...');
 
-  const stepCount = 3;
+  // to keep track of all the steps/tooltips
+  const stepCount = 4;
+  // to keep track of the current open steps/tooltips
   let currentStep = 1;
-  // Welcome tooltip
-  const welcome = $(
-    '<div id="step1" style="background-color: #85CAF6;border-radius: 5%;border: 2px solid black;display: inline-block;padding: 10px;margin-top: 26px;margin-left: -166px;"><h3 style="color: white;">Welcome to Google.com</h3><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;">Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;">Next </button><span style="float:right;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: -29px;border-width: inherit;border-style: solid;border-color: black black #85CAF6 #85CAF6;"></span></div>',
+
+  // STEP:1 Welcome tooltip
+  const welcomeTooltip = $(
+    '<div id="step1" style="background-color: rgb(133, 202, 246); border-radius: 5%; border: 2px solid black; display: inline-block; padding: 10px; margin-top: 26px; margin-left: -166px;"><h3 style="color: #163658;">Welcome to Google.com</h3><span class="cross" style="color: #080808;float: right;top: -56px;position: relative;right: -12px;border-radius: 50%;cursor: pointer;width: 17px;font-size: 12px;"> ✖ </span><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;border-radius: 6px;position: relative;left: -9px;bottom: -7px;">❮ Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;border-radius: 6px;position: relative;right: -96px;bottom: -7px;z-index: 1;">Next ❯</button><span style="float:right;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: -29px;border-width: inherit;border-style: solid;border-color: black black #85CAF6 #85CAF6;"></span></div>',
   );
-  welcome.insertBefore('.k1zIA');
+  // insert Welcome Tooltip before Google Logo
+  welcomeTooltip.insertBefore('.k1zIA');
   // ----------------*****--------------
 
-  // Images Search tooltip
-  const imageSearch = $(
-    '<div id="step2" class="hide" style="background-color: rgb(133, 202, 246); border-radius: 5%; border: 2px solid black; display: inline-block; padding: 10px; margin: 47px; position: absolute; right: 0px;"><h3 style="color: #f4f8ff;">Click Images to go to image section</h3><span id="cross" style="color: #080808;float: right;top: -1px;position: inherit;right: 0px;border-radius: 50%;cursor: pointer;width: 17px;font-size: 12px;"> ✖ </span><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;border-radius: 6px;position: absolute;left: 1px;bottom: 1px;">❮ Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;border-radius: 6px;position: absolute;right: 7px;bottom: 7px;">Next ❯</button><span style="left: 168px;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: 77px;border-width: inherit;border-style: solid;border-color: #85CAF6 #85CAF6 black black;"></span></div>',
+  // STEP:2 Images Search tooltip
+  const imageSearchTooltip = $(
+    '<div id="step2" class="hide" style="background-color: rgb(133, 202, 246); border-radius: 5%; border: 2px solid black; display: inline-block; padding: 10px; margin: 47px; position: absolute; right: 0px;"><h3 style="color: #163658;">Click Images to go to image section</h3><span class="cross" style="color: #080808;float: right;top: -1px;position: inherit;right: 0px;border-radius: 50%;cursor: pointer;width: 17px;font-size: 12px;"> ✖ </span><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;border-radius: 6px;position: absolute;left: 1px;bottom: 1px;">❮ Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;border-radius: 6px;position: absolute;right: 7px;bottom: 7px;">Next ❯</button><span style="left: 168px;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: 77px;border-width: inherit;border-style: solid;border-color: #85CAF6 #85CAF6 black black;"></span></div>',
   );
-  imageSearch.insertAfter('.gb_Zd, .gb_Ta, .gb_Kd');
+  imageSearchTooltip.insertAfter('.gb_Zd, .gb_Ta, .gb_Kd');
   $('#step2').hide();
+  // ----------------*****--------------
 
-  // Images Search tooltip
-  const searchQuery = $(
-    '<div id="step3" style="background-color: #85CAF6;border-radius: 5%;border: 2px solid black;display: inline-block;padding: 10px;margin: 16px;position: absolute;left: -187px;top: 48px;"><h3 style="color: white;">Enter a search query here and click ENTER!</h3><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;">Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;">Next </button><span style="left: 163px;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: 72px;border-width: inherit;border-style: solid;border-color: #85CAF6 #85CAF6 black black;"></span></div>',
+  // STEP:3 Search input tooltip
+  const searchInputTooltip = $(
+    '<div id="step3" style="background-color: rgb(133, 202, 246);border-radius: 5%;border: 2px solid black;display: inline-block;padding: 10px;margin: 16px;position: absolute;left: -194px;top: 48px;"><h3 style="color: #163658;">Enter a search query here and click ENTER!</h3><span class="cross" style="color: #080808;float: right;top: -58px;position: relative;right: -9px;border-radius: 50%;cursor: pointer;width: 17px;"> ✖ </span><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;border-radius: 6px;position: relative;left: -5px;bottom: -6px;">❮ Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;border-radius: 6px;position: relative;left: 241px;bottom: -6px;">Next ❯</button><span style="left: 214px;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;transform: rotate(134deg);bottom: 72px;border-width: inherit;border-style: solid;border-color: #85CAF6 #85CAF6 black black;position: relative;"></span></div>',
   );
-  searchQuery.insertAfter('.RNNXgb');
+  searchInputTooltip.insertAfter('.RNNXgb');
   $('#step3').hide();
+  // ----------------*****--------------
 
-  // handle click on back or next
+  // STEP:4 Voice Search input tooltip
+  const voiceSearchTooltip = $(
+    '<div id="step4" style="background-color: rgb(133, 202, 246);border-radius: 5%;border: 2px solid black;display: inline-block;padding: 10px;margin: 16px;position: absolute;right: -166px;top: -134px;"><h3 style="color: #163658;">Click to search by voice!</h3><span class="cross" style="color: #080808;float: right;top: -58px;position: relative;right: -9px;border-radius: 50%;cursor: pointer;width: 17px;"> ✖ </span><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;border-radius: 6px;position: relative;left: -9px;bottom: -6px;z-index: 1;">❮ Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;border-radius: 6px;position: relative;right: -96px;bottom: -6px;">Next ❯</button><span style="right: 84px;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;transform: rotate(134deg);bottom: -33px;border-width: inherit;border-style: solid;border-color: black black #85CAF6 #85CAF6;position: relative;"></span></div>',
+  );
+  voiceSearchTooltip.insertAfter('.RNNXgb');
+  $('#step4').hide();
+  // ----------------*****--------------
+
+  // handle click on back/ next/cross button on Tooltips
   $('.next').on('click', function (e) {
     e.preventDefault();
     const nextStep = currentStep + 1;
-    if (nextStep > 1 && nextStep <= stepCount) {
+    if (nextStep <= stepCount) {
       $('#step' + currentStep).hide();
       $('#step' + nextStep).show();
       currentStep = nextStep;
@@ -54,13 +59,18 @@ function startGLS() {
   $('.back').on('click', function (e) {
     e.preventDefault();
     const prevStep = currentStep - 1;
-    if (prevStep >= 1 && prevStep <= stepCount) {
+    if (prevStep >= 1) {
       $('#step' + currentStep).hide();
       $('#step' + prevStep).show();
       currentStep = prevStep;
     }
   });
-  // ----------------*****--------------
 
-  console.log('done.');
+  $('.cross').on('click', function (e) {
+    e.preventDefault();
+    for (let i = 1; i <= stepCount; ++i) {
+      $('#step' + i).remove();
+    }
+  });
+  // ----------------*****--------------
 }
