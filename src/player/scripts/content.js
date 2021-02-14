17ac5c1a0eff8f990eb63094b279f23fb839c19b
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-02-14 12:57:48 +0530

    basic working gls for google.com with 3 steps

--- src/player/scripts/content.js
+++ src/player/scripts/content.js
@@ -17,32 +17,48 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 function startGLS() {
   console.log('working on gls ...');
 
-  const stepCount = 5;
+  const stepCount = 3;
   let currentStep = 1;
   // Welcome tooltip
   const welcome = $(
-    '<div id="step1" style="background-color: #85CAF6; border-radius: 5%; border: 2px solid black; display: inline-block; padding: 10px;"><h3 style="color: white;">Welcome to Google.com</h3><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;">Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;">Next </button><span style="float:right;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: -29px;border-width: inherit;border-style: solid;border-color: black black #85CAF6 #85CAF6;"></span></div>',
+    '<div id="step1" style="background-color: #85CAF6;border-radius: 5%;border: 2px solid black;display: inline-block;padding: 10px;margin-top: 26px;margin-left: -166px;"><h3 style="color: white;">Welcome to Google.com</h3><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;">Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;">Next </button><span style="float:right;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: -29px;border-width: inherit;border-style: solid;border-color: black black #85CAF6 #85CAF6;"></span></div>',
   );
-  // welcome.insertBefore('.k1zIA');
+  welcome.insertBefore('.k1zIA');
   // ----------------*****--------------
 
-  // Welcome tooltip
+  // Images Search tooltip
   const imageSearch = $(
-    '<div id="step1" style="background-color: #85CAF6;border-radius: 5%;border: 2px solid black;display: inline-block;padding: 10px;margin: 47px;position: absolute;right: 0;"><h3 style="color: white;">Click Images to go to image section</h3><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;">Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;">Next </button><span style="left: 87px;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: 77px;border-width: inherit;border-style: solid;border-color: #85CAF6 #85CAF6 black black;"></span></div>',
+    '<div id="step2" class="hide" style="background-color: #85CAF6;border-radius: 5%;border: 2px solid black;display: inline-block;padding: 10px;margin: 47px;position: absolute;right: 0;"><h3 style="color: white;">Click Images to go to image section</h3><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;">Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;">Next </button><span style="left: 87px;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: 77px;border-width: inherit;border-style: solid;border-color: #85CAF6 #85CAF6 black black;"></span></div>',
   );
   imageSearch.insertAfter('.gb_Zd, .gb_Ta, .gb_Kd');
+  $('#step2').hide();
+
+  // Images Search tooltip
+  const searchQuery = $(
+    '<div id="step3" style="background-color: #85CAF6;border-radius: 5%;border: 2px solid black;display: inline-block;padding: 10px;margin: 16px;position: absolute;left: -187px;top: 48px;"><h3 style="color: white;">Enter a search query here and click ENTER!</h3><button class="back" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px 5px 5px 5px;cursor: pointer;">Back </button><button class="next" style="background-color: #4CAF50;border: none;color: white;padding: 5px 5px;text-align: center;text-decoration: none;display: inline-block;font-size: 12px;margin: 0px;cursor: pointer;">Next </button><span style="left: 163px;height: 16px;width: 16px;display: inline-block;background-color: #85CAF6;position: relative;transform: rotate(134deg);bottom: 72px;border-width: inherit;border-style: solid;border-color: #85CAF6 #85CAF6 black black;"></span></div>',
+  );
+  searchQuery.insertAfter('.RNNXgb');
+  $('#step3').hide();
 
   // handle click on back or next
-  $('.next').on('click', function () {
-    // insert the "newSpan" at the beginning inside all DIVs with class="cls"
+  $('.next').on('click', function (e) {
+    e.preventDefault();
     const nextStep = currentStep + 1;
-    $('#step' + nextStep).show();
+    if (nextStep > 1 && nextStep <= stepCount) {
+      $('#step' + currentStep).hide();
+      $('#step' + nextStep).show();
+      currentStep = nextStep;
+    }
   });
 
-  $('.back').on('click', function () {
-    // insert the "newSpan" at the beginning inside all DIVs with class="cls"
+  $('.back').on('click', function (e) {
+    e.preventDefault();
     const prevStep = currentStep - 1;
-    if (prevStep >= 1 && prevStep <= stepCount) $('#step' + prevStep).hide();
+    if (prevStep >= 1 && prevStep <= stepCount) {
+      $('#step' + currentStep).hide();
+      $('#step' + prevStep).show();
+      currentStep = prevStep;
+    }
   });
   // ----------------*****--------------
 
