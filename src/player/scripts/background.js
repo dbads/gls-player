d72e97020fbb5652347bdbc8bb8735a63ad5d5a4
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-02-14 18:53:30 +0530

    disable start button when gls started

--- src/player/scripts/background.js
+++ src/player/scripts/background.js
@@ -1,14 +1,18 @@
-// @ts-nocheck
-console.log('from background---');
-
 $('#startGLS').on('click', function () {
-  console.log('hello--- back');
-
   // start gls
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     console.log('sending message from back...');
-    chrome.tabs.sendMessage(tabs[0].id, 'Start GLS', function (response) {
-      console.log(response);
-    });
+    if (tabs[0].id) {
+      chrome.tabs.sendMessage(tabs[0].id, 'Start GLS', function (response) {
+        console.log(response);
+        // restrict starting gls while its already running
+        if (response.status === 'success') {
+          console.log(response.message);
+          $('#startGLS').prop('disabled', true);
+        }
+      });
+    } else {
+      console.log('No active tab.');
+    }
   });
 });
