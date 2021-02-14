3f8b5fd9c514b8ecd2575648372a6669088b8872
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-02-14 19:58:29 +0530

    show error message if opened on other websites than google.com

--- src/player/scripts/background.js
+++ src/player/scripts/background.js
@@ -2,16 +2,29 @@ $('#startGLS').on('click', function () {
   // send request to startGLS to current active tab, google.com in this case
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     if (tabs[0].id) {
-      const request = 'start GLS';
-      chrome.tabs.sendMessage(tabs[0].id, request, function (response) {
-        console.log(response);
-        // restrict starting gls while its already running
-        if (response.status === 'success') {
-          console.log(response.message);
-          $('#startGLS').prop('disabled', true);
-        }
-      });
+      // startGLS only if google.com is opened in current tab
+      if (tabs[0].url === 'https://www.google.com/') {
+        const request = 'start GLS';
+        chrome.tabs.sendMessage(tabs[0].id, request, function (response) {
+          console.log(tabs[0]);
+          console.log(response);
+          // restrict starting gls while its already running
+          if (response.status === 'success') {
+            console.log(response.message);
+            $('#startGLS').prop('disabled', true);
+          }
+        });
+      } else {
+        // show error message
+        $('#error').css('display', 'block');
+
+        // hide message about 3 seconds
+        setTimeout(() => {
+          $('#error').css('display', 'none');
+        }, 1000);
+      }
     } else {
+      // log about this
       console.log('No active tab.');
     }
   });
